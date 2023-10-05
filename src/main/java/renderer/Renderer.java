903afb3ab91094e8e3839a14586b262466309a44
Alex McCullough <alexmccullough@outlook.com>
2023-10-05 13:05:04 +0100

    fix: added checks for texture room and presence

--- src/main/java/renderer/Renderer.java
+++ src/main/java/renderer/Renderer.java
@@ -28,9 +28,12 @@ public class Renderer {
         boolean added = false;
         for (RenderBatch batch : batches) {
             if (batch.hasRoom()) {
-                batch.addSprite(sprite);
-                added = true;
-                break;
+                Texture tex = sprite.getTexture();
+                if ((tex != null && batch.hasTexture(tex) || batch.hasTextureRoom()) || tex == null) {
+                    batch.addSprite(sprite);
+                    added = true;
+                    break;
+                }
             }
         }
 
