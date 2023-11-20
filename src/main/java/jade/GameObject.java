81df99833c6a4ba31d18859f27743b9d3d461ef3
Alex McCullough <alexmccullough@outlook.com>
2023-11-20 11:20:38 +0000

    z-index added & alpha blending added

--- src/main/java/jade/GameObject.java
+++ src/main/java/jade/GameObject.java
@@ -11,17 +11,20 @@ public class GameObject {
     private String name;
     private List<Component> components = new ArrayList<>();
     public Transform transform;
+    private int zIndex;
 
     public GameObject(String name) {
         this.name = name;
         this.components = new ArrayList<>();
         this.transform = new Transform();
+        this.zIndex = 0;
     }
 
-    public GameObject(String name, Transform transform) {
+    public GameObject(String name, Transform transform, int zIndex) {
         this.name = name;
         this.components = new ArrayList<>();
         this.transform = transform;
+        this.zIndex = zIndex;
     }
 
     public <T extends Component> T getComponent(Class<T> componentClass) {
@@ -65,4 +68,7 @@ public class GameObject {
         }
     }
 
+    public int getzIndex() {
+        return this.zIndex;
+    }
 }
